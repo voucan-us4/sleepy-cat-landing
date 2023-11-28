6cab1f900ee58e51f866ef31694724ff1ed77d16
prescottc4 <svm316@icloud.com>
2023-11-28 14:53:32 +0000

    Update 4 files
    - /check.js
    - /ai.js
    - /aitest.html
    - /index.html

---
+++ ai.js
@@ -0,0 +1,3 @@
+curl https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8 \
+  -H 'Authorization: Bearer {upqa4JO8bQztaK_GHOek9eY_skl722wUBwVJncT-}' \
+  -d '{ "prompt": "Where did the phrase Hello World come from" }'
--- check.js
+++
@@ -1,8 +0,0 @@
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback" defer></script>
-curl 'https://challenges.cloudflare.com/turnstile/v0/siteverify' --data 'secret=0x4AAAAAAANvobqbfT2tJsGxi97kMHP6nKU&response=<RESPONSE>'
-{
-  "success": true,
-  "error-codes": [],
-  "challenge_ts": "2022-10-06T00:07:23.274Z",
-  "hostname": "example.com"
-}
